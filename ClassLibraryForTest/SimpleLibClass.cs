ae0e94ffbb113bdfc1b9169fdbe15e657e492002
peyba <pererushev@gmail.com>
2015-04-08 16:04:56 +0300

    added some comments

--- ClassLibraryForTest/SimpleLibClass.cs
+++ ClassLibraryForTest/SimpleLibClass.cs
@@ -1,12 +1,21 @@
 ﻿namespace ClassLibraryForTest
 {
+    /// <summary>
+    /// Simple class for test OneFileProject
+    /// </summary>
     public class SimpleLibClass
     {
+        /// <summary>
+        /// Get class version
+        /// </summary>
         public static string Version
         {
-            get { return "1.0.0"; }
+            get { return "1.0.1"; }
         }
         
+        /// <summary>
+        /// Get or set any string data
+        /// </summary>
         public string MyProperty { get; set; }
     }
 }
--- CompressClassLibrary/Program.cs
+++ CompressClassLibrary/Program.cs
@@ -7,10 +7,12 @@ namespace CompressClassLibrary
     {
         static void Main(string[] args)
         {
+            /* Get library file */
             var fileInputName = @"lib\ClassLibraryForTest.dll";
             var assembly = File.ReadAllBytes(fileInputName);
 
-            var fileOutputName = @"lib\ClassLibraryForTest.dll.deflated";
+            /* Compress and save */
+            var fileOutputName = @"lib\ClassLibraryForTest.dll.cmp";
             using (var file = File.Open(fileOutputName, FileMode.Create))
             using (var stream = new DeflateStream(file, CompressionMode.Compress))
             using (var writer = new BinaryWriter(stream))
